6997f73e3e01d42b9836fcf1cd55055d1dd999d6
kromidka <onthegodemode@gmail.com>
2024-06-13 12:01:12 +0200

    update

--- init.lua
+++ init.lua
@@ -265,7 +265,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'dracula',
         component_separators = '|',
         section_separators = '',
       },
