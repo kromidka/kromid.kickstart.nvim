ae47c112bf051cc37606c6ed8f528b8c62d742da
kromidka <onthegodemode@gmail.com>
2025-12-04 20:28:44 +0100

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -45,4 +45,21 @@ return {
 		event = "VimEnter",
 		config = true,
 	},
+	 -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'dracula',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+
+	'PProvost/vim-ps1', -- beter ps1
+	'junegunn/goyo.vim', -- Beautifle for text
+	'plasticboy/vim-markdown', -- better markdown
+	'editorconfig/editorconfig-vim', -- beter conf
+
 }
